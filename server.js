e3aca0b6821db8b0b187efd55aa09ed2237a2ac8
iamashokjat <ashokjakad@gmail.com>
2020-07-23 08:10:30 +0530

    fix database URL

--- server.js
+++ server.js
@@ -11,11 +11,9 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+  connectionString: {
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
@@ -25,7 +23,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.handleSignin(db, bcrypt)) //then calls (req,res)
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
